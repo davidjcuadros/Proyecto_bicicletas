1c84f632378c0a53e280f437333936a30fa2a31e
David Cuadros <cuadrosdavid01@gmail.com>
2024-05-27 22:04:04 +0000

    f

--- trayectos/src/main/java/com/bicicletas/trayectos/dataAccess/UsuarioRepository.java
+++ trayectos/src/main/java/com/bicicletas/trayectos/dataAccess/UsuarioRepository.java
@@ -4,6 +4,7 @@ import com.bicicletas.trayectos.modelo.*;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -12,8 +13,7 @@ public interface UsuarioRepository
     extends JpaRepository <Usuario, Integer>
     {
 
-        @Query("SELECT * FROM Reporte WHERE id= :id")
-        List<Reporte> feedUsuario(Integer id);
-
+        @Query("SELECT r FROM Reporte r WHERE r.usuario.id = :idUsuario")
+        List<Reporte> feedUsuario(@Param("idUsuario") Integer idUsuario);
 }
 
