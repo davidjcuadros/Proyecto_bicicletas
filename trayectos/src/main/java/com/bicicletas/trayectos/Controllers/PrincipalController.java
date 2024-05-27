e65dd14ac6aaaa3bd0beff631aeacb063a3ab6ea
David Cuadros <cuadrosdavid01@gmail.com>
2024-05-27 22:13:35 +0000

    f

---
+++ trayectos/src/main/java/com/bicicletas/trayectos/Controllers/PrincipalController.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+
+    @GetMapping("/")
+    public String mostrarPantallaPrincipal() {
+        return "index";
+    }
+}
